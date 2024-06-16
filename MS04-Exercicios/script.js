f1bd0f1344678853b09ab2965ab4c760dc79245d
Lucas Lino <lucaslino.martins@hotmail.com>
2024-06-16 11:36:30 -0300

    adicionando eventos na ul

---
+++ MS04-Exercicios/script.js
@@ -0,0 +1,17 @@
+const sobre = document.querySelector('#s_OBRE')
+const trailer = document.querySelector('#t_RAILER')
+const atores = document.querySelector('#a_TORES')
+const avaliacoes = document.querySelector('#a_VALI')
+
+sobre.addEventListener('click', () => {
+    window.alert('Página em construção')
+})
+trailer.addEventListener('click', () => {
+    window.alert('Página em construção')
+})
+atores.addEventListener('click', () => {
+    window.alert('Página em construção')
+})
+avaliacoes.addEventListener('click', () => {
+    window.alert('Página em construção')
+})
