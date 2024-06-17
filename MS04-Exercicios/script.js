413385280f56d9e72903456f14ddf45c4185a45c
Lucas Lino <lucaslino.martins@hotmail.com>
2024-06-17 18:58:25 -0300

    adicionando foreach nos cards

--- MS04-Exercicios/script.js
+++ MS04-Exercicios/script.js
@@ -15,3 +15,35 @@ atores.addEventListener('click', () => {
 avaliacoes.addEventListener('click', () => {
     window.alert('Página em construção')
 })
+
+
+const episodios = [
+    {
+        id: 1,
+        episodio: 'T1-01',
+        tempo: '39min'
+    },
+    {
+        id: 2,
+        episodio: 'T1-02',
+        tempo: '44min'
+    },
+    {
+        id: 3,
+        episodio: 'T1-03',
+        tempo: '49min'
+    }
+]
+const listaEP = document.getElementById('lista-ep')
+
+episodios.forEach(episodios => {
+    const cardEP = document.createElement('div')
+    cardEP.className = 'box-epsodio'
+    cardEP.innerHTML = `
+    <span>${episodios.episodio}</span>
+    <span>${episodios.tempo}</span>
+    `
+    listaEP.appendChild(cardEP)
+})
+
+
